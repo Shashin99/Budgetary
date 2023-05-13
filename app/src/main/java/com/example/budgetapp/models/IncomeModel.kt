2041fb82b8bdb6a50b3079f7ec8706ea046ae073
ShashiniH <shshani0002@gmail.com>
2023-05-13 21:59:02 +0530

    Income Function

---
+++ app/src/main/java/com/example/budgetapp/models/IncomeModel.kt
@@ -0,0 +1,11 @@
+package com.example.budgetapp.models
+
+
+data class IncomeModel(
+    var incomeName: String? = null,
+    var incomeAmount: String? = null,
+    var incomeDate: String? = null,
+    var incomeDesc: String? = null,
+
+    )
+
